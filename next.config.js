84f5aff092ed1c11284397726a8272d75bef5b92
Rene Kremer <renejon90@googlemail.com>
2023-09-29 13:24:51 +0200

    build(Next): Use standalone build configs again

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 /** @type {import('next').NextConfig} */
 const packageJson = require('./package.json');
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
 const nextConfig = {
     env: {
         NEXT_PUBLIC_APP_VERSION: packageJson.version
     },
-    output: 'export',
+    output: 'standalone',
 }
 
-module.exports = (phase, { }) => {
-    if (phase === PHASE_DEVELOPMENT_SERVER) return {
-        images: {
-            unoptimized: true
-        }, 
-        ...nextConfig
-    }
-
-    return nextConfig;
-}
+module.exports = nextConfig
