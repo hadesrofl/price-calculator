99345827bf617ac6acccc0a18ab209a3e698e27f
Rene Kremer <renejon90@googlemail.com>
2023-11-17 17:41:21 +0100

    refactor(ServiceBoard): Use tailwind css

--- src/app/components/serviceBoard/ServiceBoard.tsx
+++ src/app/components/serviceBoard/ServiceBoard.tsx
@@ -1,12 +1,5 @@
 "use client";
-import {
-  Grid,
-  Card,
-  CardContent,
-  Typography,
-  CardHeader,
-  useTheme,
-} from "@mui/material";
+import { Grid, Typography, useTheme } from "@mui/material";
 import EuroIcon from "@mui/icons-material/Euro";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
@@ -18,16 +11,12 @@ export default function ServiceBoard() {
   const { t } = useTranslation(TranslationsServiceBoard);
   const theme = useTheme();
   return (
-    <Grid container>
-      <Grid item xs={12} sm={6} md={3}>
+    <Grid container className="mx-0.5" spacing={2}>
+      <Grid item xs={11} sm={6} md={3}>
         <Link href={CostCalculationRoute}>
           <ColoredCard color={theme.palette.primary.main}>
-            <EuroIcon sx={{ marginRight: 2 }} />
-            <Typography
-              component="span"
-              variant="body1"
-              sx={{ verticalAlign: "top" }}
-            >
+            <EuroIcon className="mr-4" />
+            <Typography component="span" variant="body1" className="align-top">
               {t("CostCalculation")}
             </Typography>
           </ColoredCard>
